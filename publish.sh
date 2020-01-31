693ea2c2b8e03462f2c3e6f933b38b8440f0ba23
swpuzhang <840757167@qq.com>
2020-01-31 18:51:05 +0800

    pulish

--- publish.sh
+++ publish.sh
@@ -8,7 +8,7 @@ dotnet publish -c Release -r linux-x64 /p:PublishSingleFile=true -o ./app
 cd ../../Gateways/ApiGateway
 dotnet publish -c Release -r linux-x64 /p:PublishSingleFile=true -o ./app
 cd ../..
-cp Services/Account/app/Account /CleanGame/run/Services/Account
-cp Services/Money/app/Money /CleanGame/run/Services/Money
-cp Services/Reward/app/Reward /CleanGame/run/Services/Reward
-cp Gateways/ApiGateway/app/ApiGateway /CleanGame/run/Gateways/ApiGateway
+cp Services/Account/app/Account /CleanGame/run/Services/Account/
+cp Services/Money/app/Money /CleanGame/run/Services/Money/
+cp Services/Reward/app/Reward /CleanGame/run/Services/Reward/
+cp Gateways/ApiGateway/app/ApiGateway /CleanGame/run/Gateways/ApiGateway/
