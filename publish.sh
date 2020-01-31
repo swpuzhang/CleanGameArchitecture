195cf5c84557b13eb37732e57fb6de9204c63697
swpuzhang <840757167@qq.com>
2020-01-31 18:41:09 +0800

    发布脚本

---
+++ publish.sh
@@ -0,0 +1,14 @@
+!#/bin/bash
+cd Services/Account
+dotnet publish -c Release -r linux-x64 /p:PublishSingleFile=true -o /app
+cd ../../Services/Money
+dotnet publish -c Release -r linux-x64 /p:PublishSingleFile=true -o /app
+cd ../../Services/Reward
+dotnet publish -c Release -r linux-x64 /p:PublishSingleFile=true -o /app
+cd ../../Gateways/ApiGateway
+dotnet publish -c Release -r linux-x64 /p:PublishSingleFile=true -o /app
+cd ../..
+cp Services/Account/app/Account /CleanGame/run/Services/Account
+cp Services/Money/app/Money /CleanGame/run/Services/Money
+cp Services/Reward/app/Reward /CleanGame/run/Services/Reward
+cp Gateways/ApiGateway/app/ApiGateway /CleanGame/run/Gateways/ApiGateway
