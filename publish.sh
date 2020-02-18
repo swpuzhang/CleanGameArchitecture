9fb53a4a1298668b512f51787ff83385a8302f63
840757167@qq.com <840757167@qq.com>
2020-02-19 01:13:38 +0800

    publish 增加 RooMatch

--- publish.sh
+++ publish.sh
@@ -5,10 +5,14 @@ cd ../../Services/Money
 dotnet publish -c Release -r linux-x64 /p:PublishSingleFile=true -o ./app
 cd ../../Services/Reward
 dotnet publish -c Release -r linux-x64 /p:PublishSingleFile=true -o ./app
+cd ../../Services/RoomMatch
+dotnet publish -c Release -r linux-x64 /p:PublishSingleFile=true -o ./app
 cd ../../Gateways/ApiGateway
 dotnet publish -c Release -r linux-x64 /p:PublishSingleFile=true -o ./app
 cd ../..
 cp Services/Account/app/Account /CleanGame/run/Services/Account/
 cp Services/Money/app/Money /CleanGame/run/Services/Money/
 cp Services/Reward/app/Reward /CleanGame/run/Services/Reward/
+cp Services/RoomMatch/app/RoomMatch /CleanGame/run/Services/RoomMatch/
 cp Gateways/ApiGateway/app/ApiGateway /CleanGame/run/Gateways/ApiGateway/
+cp run/SwaggerInterface/*  /CleanGame/run/SwaggerInterface/
