c7147362ced425eda253aab1a2a299307586dc3b
840757167@qq.com <840757167@qq.com>
2020-03-03 02:24:59 +0800

    publish 脚本

--- publish.sh
+++ publish.sh
@@ -21,6 +21,13 @@ cd ../../Gateways/WSGateway
 dotnet publish -c Release -r linux-x64 /p:PublishSingleFile=true -o ./app
 
 cd ../..
+mkdir -p /CleanGame/run/Services/Account
+mkdir -p /CleanGame/run/Services/Money
+mkdir -p /CleanGame/run/Services/Reward
+mkdir -p /CleanGame/run/Services/RoomMatch
+mkdir -p /CleanGame/run/Services/Game
+mkdir -p /CleanGame/run/Gateways/ApiGateway
+mkdir -p /CleanGame/run/Gateways/WSGateway
 cp Services/Account/app/Account /CleanGame/run/Services/Account/
 cp Services/Money/app/Money /CleanGame/run/Services/Money/
 cp Services/Reward/app/Reward /CleanGame/run/Services/Reward/
